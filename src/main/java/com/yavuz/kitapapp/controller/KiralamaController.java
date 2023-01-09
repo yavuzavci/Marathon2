81eb9235df0763a0d80396b8bfb7f37631a5cf22
yavuzavci <yavuzavci@gmail.com>
2023-01-09 13:26:43 +0300

    controller katmanı eklendi

---
+++ src/main/java/com/yavuz/kitapapp/controller/KiralamaController.java
@@ -0,0 +1,17 @@
+package com.yavuz.kitapapp.controller;
+
+import com.yavuz.kitapapp.service.KasiyerService;
+import com.yavuz.kitapapp.service.KitapService;
+import com.yavuz.kitapapp.service.MusteriService;
+
+public class KiralamaController {
+    private final KasiyerService kasiyerService;
+    private final KitapService kitapService;
+    private final MusteriService musteriService;
+
+    public KiralamaController(){
+        kasiyerService = new KasiyerService();
+        kitapService = new KitapService();
+        musteriService = new MusteriService();
+    }
+}
