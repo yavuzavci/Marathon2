2a7f4560c240daf87d1f945a839f44719eee0010
yavuzavci <yavuzavci@gmail.com>
2023-01-09 13:46:28 +0300

    repositoryler güncellendi

--- src/main/java/com/yavuz/kitapapp/repository/KasiyerRepository.java
+++ src/main/java/com/yavuz/kitapapp/repository/KasiyerRepository.java
@@ -4,29 +4,53 @@ import com.yavuz.kitapapp.entity.Kasiyer;
 
 import java.util.List;
 
+import static com.yavuz.kitapapp.utility.StaticValues.*;
+
 public class KasiyerRepository implements ICrud<Kasiyer>{
     @Override
     public void save(Kasiyer kasiyer) {
-
+        KASIYER_LISTESI.add(kasiyer);
     }
 
     @Override
     public void update(Kasiyer kasiyer) {
-
+        for (int i = 0; i < KASIYER_LISTESI.size(); i++) {
+            if(KASIYER_LISTESI.get(i).getId().equals(kasiyer.getId())){
+                Kasiyer eskiKasiyer = KASIYER_LISTESI.get(i);
+                KASIYER_LISTESI.set(i,kasiyer);
+                KITAP_LISTESI.stream()
+                        .filter(k -> k.getKiralayanKasiyer().equals(eskiKasiyer))
+                        .forEach(k -> k.setKiralayanKasiyer(kasiyer));
+                break;
+            }
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        for (int i = 0; i < KASIYER_LISTESI.size(); i++) {
+            if(KASIYER_LISTESI.get(i).getId().equals(id)){
+                Kasiyer silinenKasiyer = KASIYER_LISTESI.get(i);
+                KASIYER_LISTESI.remove(i);
+                KITAP_LISTESI.stream()
+                        .filter(k -> k.getKiralayanKasiyer().equals(silinenKasiyer))
+                        .forEach(k -> k.setKiralayanKasiyer(null));
+                break;
+            }
+        }
     }
 
     @Override
     public List<Kasiyer> findAll() {
-        return null;
+        return KASIYER_LISTESI;
     }
 
     @Override
     public Kasiyer findById(Long id) {
+        for (Kasiyer kasiyer : KASIYER_LISTESI) {
+            if (kasiyer.getId().equals(id))
+                return kasiyer;
+        }
         return null;
     }
 }
--- src/main/java/com/yavuz/kitapapp/repository/MusteriRepository.java
+++ src/main/java/com/yavuz/kitapapp/repository/MusteriRepository.java
@@ -4,29 +4,53 @@ import com.yavuz.kitapapp.entity.Musteri;
 
 import java.util.List;
 
+import static com.yavuz.kitapapp.utility.StaticValues.*;
+
 public class MusteriRepository implements ICrud<Musteri> {
     @Override
     public void save(Musteri musteri) {
-
+        MUSTERI_LISTESI.add(musteri);
     }
 
     @Override
     public void update(Musteri musteri) {
-
+        for (int i = 0; i < MUSTERI_LISTESI.size(); i++) {
+            if(MUSTERI_LISTESI.get(i).equals(musteri)){
+                Musteri eskiMusteri = MUSTERI_LISTESI.get(i);
+                MUSTERI_LISTESI.set(i,musteri);
+                KITAP_LISTESI.stream()
+                        .filter(k -> k.getKiralayanMusteri().equals(eskiMusteri))
+                        .forEach(k-> k.setKiralayanMusteri(musteri));
+                break;
+            }
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        for (int i = 0; i < MUSTERI_LISTESI.size(); i++) {
+            if(MUSTERI_LISTESI.get(i).getId().equals(id)){
+                Musteri silinenMusteri = MUSTERI_LISTESI.get(i);
+                MUSTERI_LISTESI.remove(i);
+                KITAP_LISTESI.stream()
+                        .filter(k -> k.getKiralayanMusteri().equals(silinenMusteri))
+                        .forEach(k-> k.setKiralayanMusteri(null));
+                break;
+            }
+        }
     }
 
     @Override
     public List<Musteri> findAll() {
-        return null;
+        return MUSTERI_LISTESI;
     }
 
     @Override
     public Musteri findById(Long id) {
+        for(Musteri musteri : MUSTERI_LISTESI){
+            if(musteri.getId().equals(id))
+                return musteri;
+        }
         return null;
     }
 }
